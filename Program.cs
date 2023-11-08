9da78706abd510b15c48ad20b96adc12067c66fd
Caleb Wolthers <w0473807@nscc.ca>
2023-11-08 15:17:20 -0400

    Finished Work
    Working map,
    Working scale,
    Working border and border scale,
    Coloured tiles,
    Legend

--- Program.cs
+++ Program.cs
@@ -9,27 +9,54 @@ namespace RPGmap_CalebWolthers_20231101
     internal class Program
     {
 
+
         static int currentRow = 0;
         static int currentCol = 0;
 
-        static int printNum = 0;
+        static int width;
+        static int height;
 
-    
 
 
         static void Main(string[] args)
         {
 
 
+
+            width = map.GetLength(1);
+            height = map.GetLength(0);
+
+            Console.WriteLine("Current map width: " + width);
+            Console.WriteLine("Current map height: " + height);
+
+
+
+            DisplayLegend();
+
             currentRow = 0;
             currentCol = 0;
 
-            DrawMapScale(3);
+            DisplayMap();
 
             currentRow = 0;
             currentCol = 0;
 
-            DrawMap();
+            DisplayMap(2);
+
+            currentRow = 0;
+            currentCol = 0;
+
+            DisplayMap(3);
+
+            currentRow = 0;
+            currentCol = 0;
+
+            DisplayMap(4);
+
+            currentRow = 0;
+            currentCol = 0;
+
+            DisplayMap(5);  
 
             Console.ReadKey();
 
@@ -38,68 +65,155 @@ namespace RPGmap_CalebWolthers_20231101
         }
 
 
-        static void DrawMapScale(int scale)
+        static void DisplayMap(int scale)
         {
-            while (currentRow < 12)
+
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("+");
+            for (int i = 0; i < width * scale; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+            while (currentRow < height)
             {
                 for (int j = 0; j < scale; j++)
                 {
-                    while (currentCol < 30)
+                    while (currentCol < width)
                     {
+                        if (currentCol == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("|");
+                        }
                         for (int k = 0; k < scale; k++)
                         {
+                            if (map[currentRow, currentCol] == '~')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                            }
+                            else if (map[currentRow, currentCol] == '*')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            }
+                            else if (map[currentRow, currentCol] == '^')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                            }
+                            else if (map[currentRow, currentCol] == '`')
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+
+
                             Console.Write(map[currentRow, currentCol]);
                         }
                         currentCol++;
                     }
                     currentCol = 0;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("|");
                     Console.WriteLine();
                 }
                 currentRow++;
                 currentCol = 0;
             }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("+");
+            for (int i = 0; i < width * scale; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
 
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
-
-
-        static void DrawMap()
+        static void DisplayLegend()
         {
-            Console.WriteLine("+------------------------------+");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("+--------------+");
+            Console.WriteLine("| ^ = mountain |");
+            Console.WriteLine("| ` = grass    |");
+            Console.WriteLine("| ~ = water    |");
+            Console.WriteLine("| * = trees    |");
+            Console.WriteLine("+--------------+");
+        }
+        // map legend:
+        // ^ = mountain
+        // ` = grass
+        // ~ = water
+        // * = trees
 
-            while (currentCol < 30)
+
+        static void DisplayMap()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("+");
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+
+            while (currentCol < width)
             {
                 if (currentCol == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("|");
                 }
+                if (map[currentRow, currentCol] == '~')
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (map[currentRow, currentCol] == '*')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (map[currentRow, currentCol] == '^')
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (map[currentRow, currentCol] == '`')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+
                 Console.Write(map[currentRow, currentCol]);
                 currentCol++;
 
-                if (currentCol >= 30)
+                if (currentCol >= width)
                 {
                     currentRow++;
-                    if (currentRow < 12)
+                    if (currentRow < height)
                     {  
                         currentCol = 0;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write("|");
                         Console.WriteLine();
                     }
                     else 
                         { 
-                            currentCol = 32;
-                            Console.Write("|");
+                            currentCol = width + 2;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("|");
                         }
                 }
+
             }
             Console.WriteLine();
-            Console.WriteLine("+------------------------------+");
+            Console.Write("+");
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
-
-
- 
-
-
 
 
 
@@ -108,8 +222,39 @@ namespace RPGmap_CalebWolthers_20231101
 
 
         static char[,] map = new char[,] // dimensions defined by following data:
+   {
+        {'~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~'},
+        {'~','~','`','`','`','`','`','~','~','~','~','~','~','~','*','*','`','~','~','~','~','~','~','~','~','`','`','~','~','~','~','~','~','~','~','~','~','~'},
+        {'~','`','`','*','*','`','`','`','`','~','~','~','~','~','`','`','`','~','~','~','~','~','~','~','~','`','`','^','^','^','^','^','^','^','~','~','~','~'},
+        {'~','`','`','*','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','*','*','`','`','~','~','`','`','^','^','^','~','~','~'},
+        {'~','~','`','`','`','^','`','^','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','*','`','^','^','^','`','`','`','`','^','~','~','~'},
+        {'~','~','~','~','~','^','^','^','~','~','~','~','~','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','^','`','`','`','^','~','~','~'},
+        {'~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','^','`','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','~','~','~','~'},
+        {'^','`','`','`','`','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~'},
+        {'^','^','^','^','`','`','`','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','^','^','`','`','`','*','~','~','~','~','~','~'},
+        {'`','^','^','^','`','`','`','`','`','`','`','*','*','`','~','~','~','~','~','~','~','~','*','*','`','`','`','`','`','`','`','*','*','~','~','~','~','~'},
+        {'`','~','^','^','^','`','`','`','`','`','*','*','*','`','~','~','~','~','~','~','~','~','*','*','*','`','`','`','`','`','`','`','*','~','~','~','~','~'},
+        {'~','~','^','^','^','`','`','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`'},
+        {'~','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','`','*','*'},
+        {'`','`','`','`','*','*','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','`','*','*'},
+        {'`','`','`','*','*','*','*','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','`','`','*','*','*'},
+        {'`','`','`','`','*','*','*','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','`','`','`','`','*','*','*','*'},
+
+
+
+   }; 
+
+
+
+
+
+
+
+
+
+   /*     static char[,] map = new char[,] // dimensions defined by following data:
     {
-        {'S','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+        {'^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`'},
         {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`'},
         {'^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
@@ -120,8 +265,8 @@ namespace RPGmap_CalebWolthers_20231101
         {'`','`','`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`'},
         {'`','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','E'},
-    };
+        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
+    }; */
 
         // usage: map[y, x]
 
